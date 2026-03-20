72d0de0f251c9dcd3937a34075d4d12ab68049e9
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-03-20 18:06:10 +0000

    n

--- gnupg-script.sh
+++ gnupg-script.sh
@@ -2,3 +2,5 @@ uname -a#da información sobre la computadora
 which gpg #ubicación del binario de gpg
 gpg --version #da la version e información adicional
 gpg --full-generate-key #genero la llave publica y privada 
+gpg --list-keys #explorar el llavero
+gpg --armor -export #exportar llave
