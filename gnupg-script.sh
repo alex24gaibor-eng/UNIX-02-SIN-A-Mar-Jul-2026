d9e83477b9a1ce0ec68d6a1feb75d4753e418679
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-03-28 04:26:06 +0000

    wip:verify if the document exists and its content

--- gnupg-script.sh
+++ gnupg-script.sh
@@ -5,4 +5,4 @@ gpg --full-generate-key #genero la llave publica y privada
 gpg --list-keys #explorar el llavero
 gpg --armor -export #exportar llave
 gpg --list-secret-keys --keyd-format=long # para listar las llaves privadas
-gpg 
+cat doc_no_cifrado.txt #comprobar si el documento esta creado
