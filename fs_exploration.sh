229ba089b9eef6b4b1c89569e238e8d567c36416
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-04-02 13:26:08 +0000

    wip:sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"

---
+++ fs_exploration.sh
@@ -0,0 +1,11 @@
+#It is used in systems like Linux or Unix to change directories.
+cd /
+#it´s a variant of the ls comand, which is used to list files and folders 
+ls -F
+#It is used on Debian/Ubuntu-based systems to update already installed programs
+sudo apt update 
+#Updates the list of available software and their versions
+sudo apt upgrade
+#It updates the software already installed on your system
+sudo apt install parted 
+#Install a tool that allows you to create, delete, and manage your hard drive partitions from the terminal
