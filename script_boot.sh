3725451b6e81713db0a0e174154e170176647515
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-04-09 12:43:36 +0000

    wip show different routes

--- script_boot.sh
+++ script_boot.sh
@@ -1,3 +1,7 @@
 mkdir #create a directory
 cd proyecto/ #change for the project using a relative path
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #change using an absolute path
+echo $HOME #Show your personal folder
+echo $BASH #Shows the path of the Bash interpreter
+echo $PAHT #Shows paths where the system looks for commands
+cd/usr/local #Change to the /usr/local directory
