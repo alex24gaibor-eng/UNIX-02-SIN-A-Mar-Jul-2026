2ee603ec4077e63b8ec07e81cab4c88cd855dc81
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-04-09 12:58:32 +0000

    wip commands with ls

--- script_boot.sh
+++ script_boot.sh
@@ -5,3 +5,10 @@ echo $HOME #Show your personal folder
 echo $BASH #Shows the path of the Bash interpreter
 echo $PAHT #Shows paths where the system looks for commands
 cd/usr/local #Change to the /usr/local directory
+cd /workspaces #Change to the /workspaces directory
+ls -lai #List files with details, including hidden files and inodes
+ls -l -a -i ##Hacer lo mismo que el comando anterior
+man ls #Displays the manual for the ls command
+ls --all #List all files, including hidden ones
+. #virtual directory
+.. #parent directory
