26736465fb662263c5253eb81af7e05f609e2991
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-04-16 12:56:55 +0000

    wip used the commands mddir to created the folder called -rf

--- calculadora/boot_exploration.sh
+++ calculadora/boot_exploration.sh
@@ -1,2 +1,4 @@
 ls -l -a -h
 ls -lah This comandos #these commands are used to see everything in a folder with details
+mkdir -- -rf #created the folder called -rf
+rmdir -- -rf #delete that folder (if it is empty).
