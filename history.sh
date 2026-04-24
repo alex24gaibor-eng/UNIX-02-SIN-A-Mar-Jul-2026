5395a6eeee18b3353926e3fee2ff03cd2b39b859
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-04-24 17:25:51 +0000

    wip:first commans

---
+++ history.sh
@@ -0,0 +1,9 @@
+    1  git fech upstream
+    2  git fetch upstream
+    3  git checkout -b eval_p2_1_v1 upstream/eval_p2_1_v1
+    4  git checkout -b eval_p2_1V1 upstream/eval_p2_1_V1
+    5  git checkout -b eval_p2_1_g2 upstream/eval_p2_1_g2
+    6  history
+    7  git push -u origin eval_p2_1_g2
+    8  history
+    
