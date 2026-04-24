ff04ea2e7bc0f949972c6afd541aa21a4d43c188
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-04-24 17:32:12 +0000

    wip:  problem 1

--- history.sh
+++ history.sh
@@ -6,4 +6,17 @@
     6  history
     7  git push -u origin eval_p2_1_g2
     8  history
-    
+    #Problema 1
+        9  cd /kepler
+   10  ls
+   11  mvls
+   12  ls
+   13  cd evaluation
+   14  ls
+   15  cd kepler
+   16  mv docs registros
+   17  mkdir plantillas
+   18  git add .
+   19  git commit -m "Fix problema 1: estructura de directorios corregida"
+   20  ls
+   21  history
