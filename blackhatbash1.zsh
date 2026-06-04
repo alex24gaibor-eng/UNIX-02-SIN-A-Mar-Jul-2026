bc3c0b1bc832a123a0fc650a41f56a58935ca69c
alex24gaibor-eng <alex24gaibor@gmail.com>
2026-06-04 12:56:30 +0000

    wip:blackhatbash

---
+++ blackhatbash1.zsh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+bash --version
+#
+env
+#
+echo ${SHELL}
+#
+echo ${RANDOM}
+#
+echo ${UID}
+#
+echo ${OSTYPE}
+#
+man ls
+#
+ls --help
+#
+ps -e -f
+#
+ps -ef
+#
+df --human-readable
+#
+bash -r blackhatbash1.sh
+#
+bash -x blackhatbash1.sh
+#
